837350ff63d05bc912f5ae9a92e233b6eede977b
kammu2000 <deepanshuupadhyay1310@gmail.com>
2026-07-15 03:50:46 +0530

    [MINI-GIT] fix: reviewed and finalized parser changes

--- include/minigit/cli/parser.hpp
+++ include/minigit/cli/parser.hpp
@@ -5,12 +5,12 @@
 
 namespace minigit::cli {
 
-struct ParsedCli
-{
-    std::string command;
-    std::vector<std::string> args;
-};
+    struct ParsedCli
+    {
+        std::string command;
+        std::vector<std::string> args;
+    };
 
-[[nodiscard]] ParsedCli parse(int argc, char* argv[]);
+    [[nodiscard]] ParsedCli parse(int argc, char* argv[]);
 
 } // namespace minigit::cli
--- src/cli/parser.cpp
+++ src/cli/parser.cpp
@@ -5,23 +5,24 @@
 
 namespace minigit::cli {
 
-ParsedCli parse(int argc, char* argv[])
-{
-    if (argc < 2)
+    ParsedCli parse(int argc, char* argv[])
+
     {
-        format::print_error(std::cerr, "No command specified");
-        std::exit(1);
+        if (argc < 2)
+        {
+            format::print_error(std::cerr, "No command specified");
+            std::exit(1);
+        }
+
+        ParsedCli parsed;
+        parsed.command = argv[1];
+
+        for (int i = 2; i < argc; ++i)
+        {
+            parsed.args.emplace_back(argv[i]);
+        }
+
+        return parsed;
     }
-
-    ParsedCli parsed;
-    parsed.command = argv[1];
-
-    for (int i = 2; i < argc; ++i)
-    {
-        parsed.args.emplace_back(argv[i]);
-    }
-
-    return parsed;
-}
 
 } // namespace minigit::cli
